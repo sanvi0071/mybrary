6f25346f6608f62bf2731db7031df3ff71f61635
sanvi0071 <sanvi0071@gmail.com>
2021-10-16 20:45:56 +0530

    Initial Author Routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const router = express.Router()
-
 router.get('/', (req, res) => {
      res.render('index')
 })
--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 if (process.env.DATABASE_URL !== 'production') {
-    require('dotenv').config({path:'.env'});
-    
-}
-
+    require('dotenv').config({path:'.env'})
+    }
 
 const express = require('express') 
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
 
 const mongoose = require('mongoose')
+const { parse } = require('dotenv')
 mongoose.createConnection(process.env.DATABASE_URL, {
     useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 app.use('/', indexRouter)
-
+app.use('/authors', authorRouter)
 app.listen(process.env.PORT || 3000)
